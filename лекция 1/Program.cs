a755a54cc1f8ecfb4c01c071bac7fa9420dc4e31
Shamil Magomedov <shmagomedov1997@mail.ru>
2023-06-10 23:28:07 +0300

    задача с собакой

--- лекция 1/Program.cs
+++ лекция 1/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int count = 0; 
+int distance = 8000;
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+int friend = 2;
+
+while (distance > 10)
+{
+    if (friend == 1)
+    {
+        int time = distance / (firstFriendSpeed + dogSpeed);
+        distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+        friend = 2;
+    }
+    else
+    {
+        int time = distance / (secondFriendSpeed + dogSpeed);
+        distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+        friend = 1;
+    }
+    count++;
+}
+Console.Write("Собака бежит ");
+Console.Write(count);
+Console.Write(" раз");
